3959ec65a3b72347debb31321174f25a8044448c
Jason-Holliday <105673390+Jason-Holliday@users.noreply.github.com>
2025-04-01 14:16:44 +0200

    Initial commit

---
+++ db.js
@@ -0,0 +1,124 @@
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// Überprüfe, ob alle benötigten Umgebungsvariablen gesetzt sind
+const requiredEnv = ["MYSQL_HOST", "MYSQL_USER", "MYSQL_PASSWORD", "MYSQL_DATABASE"];
+requiredEnv.forEach(envVar => {
+    if (!process.env[envVar]) {
+        console.error(`❌ Fehler: ${envVar} ist nicht in der .env-Datei gesetzt!`);
+        process.exit(1);
+    }
+});
+
+// Erstelle die DB-Verbindung
+const createDBConnection = async () => {
+    const db = await mysql.createPool({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        port: process.env.MYSQL_PORT || 3306,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    });
+    return db;
+};
+
+
+// Funktion zum Speichern eines Projekts
+const saveProject = async (projectData) => {
+    const { title, description, imgUrl, techUsed, githubUrl, liveDemoLink } = projectData;
+
+    console.log("Projekt Daten:", projectData); 
+
+    // MySQL-Query vorbereiten
+    const query = `
+        INSERT INTO projects (title, description, image_url, tech_used, github_rep_link, live_demo_link)
+        VALUES (?, ?, ?, ?, ?, ?)
+    `;
+
+    try {
+        const dbConnection = await createDBConnection();  // Hol die globale DB-Verbindung
+        const [result] = await dbConnection.execute(query, [title, description, imgUrl, techUsed, githubUrl, liveDemoLink]);
+
+        console.log("✅ Projekt erfolgreich gespeichert, ID:", result.insertId);
+        return { success: true, insertId: result.insertId };
+    } catch (error) {
+        console.error("❌ Fehler beim Speichern des Projekts:", error.message);
+        return { success: false, error: error.message };
+    }
+};
+
+async function getProjects(searchTerm) {
+    const dbConnection = await createDBConnection();
+    
+    if (!dbConnection) {
+        console.error("❌ Keine gültige DB-Verbindung");
+        return [];
+    }
+
+    const query = 'SELECT * FROM projects WHERE title LIKE ?';
+    try {
+        const [results] = await dbConnection.execute(query, [`%${searchTerm}%`]);
+        return results;
+    } catch (error) {
+        console.error("❌ Fehler bei der Abfrage:", error.message);
+        return [];
+    }
+}
+
+// Projekt aktualisieren
+async function updateProject(projectId, updatedProject) {
+    const { title, description, imgUrl, techUsed, githubUrl, liveDemoLink } = updatedProject;
+
+    const query = `
+        UPDATE projects 
+        SET title = ?, description = ?, image_url = ?, tech_used = ?, github_rep_link = ?, live_demo_link = ? 
+        WHERE id = ?
+    `;
+    const values = [title, description, imgUrl, techUsed, githubUrl, liveDemoLink, projectId];
+
+    try {
+        const dbConnection = await createDBConnection(); // Hol die globale DB-Verbindung
+        const [result] = await dbConnection.execute(query, values);
+        return result;
+    } catch (error) {
+        console.error("❌ Fehler beim Aktualisieren des Projekts:", error.message);
+        throw error; // Fehler weitergeben, um ihn im Aufrufer zu behandeln
+    }
+}
+
+// Projekt löschen
+async function deleteProject(projectId) {
+    const query = `
+        DELETE FROM projects 
+        WHERE id = ?
+    `;
+    
+    try {
+        const dbConnection = await createDBConnection(); // Hol die globale DB-Verbindung
+        const [result] = await dbConnection.execute(query, [projectId]);
+
+        if (result.affectedRows > 0) {
+            console.log(`✅ Projekt mit ID ${projectId} erfolgreich gelöscht.`);
+            return { success: true };
+        } else {
+            console.log(`❌ Kein Projekt mit ID ${projectId} gefunden.`);
+            return { success: false, message: 'Projekt nicht gefunden.' };
+        }
+    } catch (error) {
+        console.error("❌ Fehler beim Löschen des Projekts:", error.message);
+        throw error; // Fehler weitergeben, um ihn im Aufrufer zu behandeln
+    }
+}
+
+
+module.exports = { 
+    saveProject, 
+    updateProject,
+    getProjects,
+    deleteProject
+};
---
+++ server.js
@@ -0,0 +1,165 @@
+const express = require('express');
+const multer = require('multer');
+const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const { uploadImage } = require('./cloudinary'); // Import der Cloudinary-Funktion
+const { saveProject, updateProject, getProjects, deleteProject } = require('./db'); // Import der Datenbank-Funktionen
+
+// Lade Umgebungsvariablen aus der .env-Datei
+dotenv.config();
+
+const app = express(); // Erstellt den Express-Server
+
+// CORS-Konfiguration
+const corsOptions = {
+    origin: ['https://jasonholliday.dev', 'https://www.jasonholliday.dev'], // Erlaube nur Anfragen von deiner Domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Erlaubte Methoden
+};
+
+// Middleware für CORS und statische Dateien
+app.use(cors(corsOptions));  // CORS mit der richtigen Konfiguration
+app.use(express.static('public')); // Stellt Dateien aus dem 'public'-Ordner bereit
+
+
+// Middleware zum Parsen von URL-codierten Daten (für FormData)
+app.use(express.urlencoded({ extended: true }));  // Hinzugefügt, um FormData zu unterstützen
+app.use(express.json()); // Middleware zum Parsen von JSON-Daten
+
+// E-Mail senden
+app.post("/send-email", async (req, res) => {
+    const { email, firstName, lastName, message } = req.body;
+
+    if (!email || !firstName || !lastName || !message) {
+        return res.status(400).json({ error: "Alle Felder sind erforderlich!" });
+    }
+
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: false, // true für 465, false für andere Ports
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.RECEIVER_EMAIL,
+            to: process.env.RECEIVER_EMAIL,
+            subject: `📩 Rückmeldung zur Bewerbung von  (${email})`,
+            text: `Absender: ${email}\n\nNachricht:\n${message}`,
+            html: `
+                    <h2>Neue Nachricht</h2>
+                    <p><strong>Von:</strong> ${firstName} ${lastName} (${email})</p>
+                    <p><strong>Nachricht:</strong></p>
+                    <p>${message}</p>
+            `,
+            replyTo: email
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+        console.log("E-Mail gesendet:", info.response);
+
+        res.json({ success: true, message: "E-Mail erfolgreich gesendet!" });
+    } catch (error) {
+        console.error("Fehler beim Senden der E-Mail:", error);
+        res.status(500).json({ error: "Fehler beim Senden der E-Mail" });
+    }
+});
+
+// Middleware zum Hochladen von Bildern
+const upload = multer();
+
+// Route zum Hochladen von Bildern
+app.post('/upload', upload.single('image'), async (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: 'Kein Bild hochgeladen!' });
+    }
+
+    const folder = 'portfolio/img'; // Zielordner für das Bild
+
+    try {
+        const secureUrl = await uploadImage(req.file.buffer, folder);
+        res.json({ secure_url: secureUrl });
+    } catch (error) {
+        console.error('Fehler beim Hochladen:', error);
+        res.status(500).json({ message: 'Fehler beim Hochladen des Bildes', error: error.message });
+    }
+});
+
+
+app.get('/admin', (req, res) => {
+    res.sendFile(__dirname + '/public/admin/admin.html');
+});
+
+
+
+// Route zum Abrufen der Projekte
+app.get('/projects', async (req, res) => {
+    try {
+        const searchTerm = req.query.search || ''; // Falls kein Suchbegriff vorhanden, leere Zeichenkette
+        const projects = await getProjects(searchTerm);
+        res.json(projects);
+    } catch (error) {
+        console.error('Fehler beim Abrufen der Projekte:', error);
+        res.status(500).json({ message: 'Fehler beim Abrufen der Projekte', error: error.message });
+    }
+});
+
+// Route zum Speichern eines Projekts
+app.post('/save-project', async (req, res) => {
+    try {
+        const projectData = req.body;
+        const results = await saveProject(projectData);
+        res.status(201).json({ message: 'Projekt erfolgreich gespeichert', projectId: results.insertId });
+    } catch (error) {
+        console.error('Fehler beim Speichern des Projekts:', error);
+        res.status(500).json({ message: 'Fehler beim Speichern des Projekts', error: error.message });
+    }
+});
+
+// Route zum Aktualisieren eines Projekts
+app.put('/projects/:id', async (req, res) => {
+    const { id } = req.params;
+    const updatedProject = req.body;
+
+    try {
+        await updateProject(id, updatedProject);
+        res.json({ message: "Projekt erfolgreich aktualisiert!" });
+    } catch (error) {
+        console.error('Fehler beim Aktualisieren des Projekts:', error);
+        res.status(500).json({ message: "Fehler beim Aktualisieren des Projekts", error: error.message });
+    }
+});
+
+// Route zum Löschen eines Projekts
+app.delete('/projects/:id', async (req, res) => {
+    const projectId = req.params.id;
+
+    try {
+        const result = await deleteProject(projectId);
+        if (result.success) {
+            res.json({ message: 'Projekt erfolgreich gelöscht.' });
+        } else {
+            res.status(404).json({ message: result.message });
+        }
+    } catch (error) {
+        console.error('Fehler beim Löschen des Projekts:', error);
+        res.status(500).json({ message: 'Fehler beim Löschen des Projekts.' });
+    }
+});
+
+// Standard-Route, um sicherzustellen, dass der Server läuft
+app.get("/", (req, res) => {
+    res.send("Server läuft!");
+});
+
+// Definiere den HOST und PORT
+const port = process.env.PORT || 3000; 
+
+// Server starten
+app.listen(port, function () {
+    console.log(`Server läuft auf Port ${port}`);
+});
